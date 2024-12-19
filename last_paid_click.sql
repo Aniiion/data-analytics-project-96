e12d6b7398b3b1119e9c3dc9b09200d99050d5ca
Aniiion <167976508+Aniiion@users.noreply.github.com>
2024-12-19 16:17:13 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,14 +1,46 @@
+-- Запрос для last_paid_click.csv топ-10 записей
 with tab as (
-select l.visitor_id, visit_date, source as utm_source,
-row_number() over(partition by l.visitor_id order by s.visit_date desc) as rn, 
-medium as utm_medium, campaign as utm_campaign, lead_id, created_at, amount, closing_reason,
-status_id
-from sessions s 
-left join leads l on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
-where medium in ('cpc', 'cpm','cpa', 'youtube', 'cpp', 'tg','social'))
-select visitor_id, visit_date, utm_source, utm_medium, utm_campaign, lead_id, created_at,
-amount, closing_reason, status_id
+    select
+        l.visitor_id,
+        visit_date,
+        source as utm_source,
+        medium as utm_medium,
+        campaign as utm_campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id,
+        row_number()
+            over (
+                partition by l.visitor_id
+                order by s.visit_date desc
+            )
+            as rn
+    from sessions as s
+    left join
+        leads as l
+        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+    where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+)
+
+select
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
 from tab
 where rn = '1'
-order by amount desc nulls last, visit_date, utm_source, utm_medium, utm_campaign
+order by
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc
 limit 10;
