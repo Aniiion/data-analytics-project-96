e2166756e5177f53662b7fa96fc3776b6dfc8fae
Aniiion <167976508+Aniiion@users.noreply.github.com>
2025-01-18 20:43:51 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -2,26 +2,26 @@
 with tab as (
     select
         l.visitor_id,
-        visit_date,
-        source as utm_source,
-        medium as utm_medium,
-        campaign as utm_campaign,
-        lead_id,
-        created_at,
-        amount,
-        closing_reason,
-        status_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
         row_number()
-            over (
-                partition by l.visitor_id
-                order by s.visit_date desc
-            )
-            as rn
+        over (
+            partition by l.visitor_id
+            order by s.visit_date desc
+        )
+        as rn
     from sessions as s
     left join
         leads as l
         on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
-    where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 )
 
 select
